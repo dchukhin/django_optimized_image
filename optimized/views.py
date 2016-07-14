d36f7ddd95a8e319d63c465f29813308ce83fa9b
Dmitriy Chukhin <dchukhin@gmail.com>
2016-07-14 16:25:33 -0400

    initial commit

---
+++ optimized/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'optimized/index.html')
+#                           optimized/index.html
