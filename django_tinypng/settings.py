9ab165f9e6b8834efe4b5df1265c364754d43ec1
Kia Lam <digitalanime@gmail.com>
2016-07-15 11:29:03 -0400

    database connection to heroku

--- django_tinypng/settings.py
+++ django_tinypng/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,7 +87,8 @@ DATABASES = {
         'NAME': 'django_tinypng',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
