edab8de9e493be6d985cad01266bcff18c644efb
Dmitriy Chukhin <dchukhin@gmail.com>
2016-07-15 10:12:12 -0400

    show model in view

--- optimized/views.py
+++ optimized/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
-    return render(request, 'optimized/index.html')
+    return render(request, 'optimized/index.html', {'the_models': models.BlogPostOrSomething.objects.all()})
