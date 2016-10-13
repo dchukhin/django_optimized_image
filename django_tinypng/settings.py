e192b23ff808b0b9a12330c6306f950c4f257e05
Dmitriy Chukhin <dchukhin@gmail.com>
2016-10-13 16:27:00 -0400

    add staticfiles to urls file

--- django_tinypng/settings.py
+++ django_tinypng/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'public', 'static')
