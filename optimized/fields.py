24004da9dd405bbd276ea809bee73d63a4a74ffc
Dmitriy Chukhin <dchukhin@gmail.com>
2016-10-14 13:17:39 -0400

    add optimized_url property and update OptimizedNotOptimized objects on image update

--- optimized/fields.py
+++ optimized/fields.py
@@ -2,10 +2,35 @@ import tinify
 
 from django.conf import settings
 from django.db.models import ImageField
+from django.db.models.fields.files import ImageFieldFile
+
+
+class OptimizedImageFieldFile(ImageFieldFile):
+
+    @property
+    def optimized_url(self):
+        self._require_file()
+        from .utils import get_optimized_url
+        return get_optimized_url(self.instance, self.field.name)
+
+    def delete(self, save=True):
+        """Remove the OptimizedNotOptimized object on image deletion."""
+        from .models import OptimizedNotOptimized
+        optimized_data_obj = OptimizedNotOptimized.objects.filter(
+            instance_model=self.instance._meta.label,
+            instance_pk=self.instance.pk,
+            field_name=self.field.name
+            )
+        super().delete(save)
+        if optimized_data_obj.exists():
+            optimized_data_obj[0].delete()
+
 
 
 class OptimizedImageField(ImageField):
     """An ImageField that gets optimized on save() using tinyPNG."""
+    attr_class = OptimizedImageFieldFile
+
     def pre_save(self, model_instance, add):
         """Optimize the image being saved and create an instance in db linking to the image."""
         image_file = getattr(model_instance, self.name)
@@ -16,15 +41,29 @@ class OptimizedImageField(ImageField):
 
             # Add checking in here?
             from .models import OptimizedNotOptimized
-            new_object, created = OptimizedNotOptimized.objects.update_or_create(
+            new_object, created = OptimizedNotOptimized.objects.get_or_create(
                 instance_model=model_instance._meta.label,
                 instance_pk=model_instance.pk,
                 field_name=self.name,
-                url=getattr(model_instance, self.name),
-                optimized_url=s3_response.location
             )
+            new_object.url = getattr(model_instance, self.name)
+            new_object.optimized_url = s3_response.location
             new_object.save()
         return super().pre_save(model_instance, add)
+
+    def save_form_data(self, instance, data):
+        """Remove the OptimizedNotOptimized object on clearing the image."""
+        from .models import OptimizedNotOptimized
+        # If we are clearing the image
+        if data == False:
+            optimized_data_obj = OptimizedNotOptimized.objects.filter(
+                instance_model=instance._meta.label,
+                instance_pk=instance.pk,
+                field_name=self.name
+            )
+            if optimized_data_obj.exists():
+                optimized_data_obj[0].delete()
+        super().save_form_data(instance, data)
 
 def save_to_s3(image_file):
     tinify.key = settings.TINYPNG_KEY
