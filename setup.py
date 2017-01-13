77cb43205c16f880a4827629e8cf10312b061c8d
Dmitriy Chukhin <dchukhin@gmail.com>
2017-01-13 10:15:16 -0500

    add setup.py and setup.cfg to repo

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='optimized-image',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app that allows for optimization of images.',
+    long_description=README,
+    install_requires=['tinify'],
+    url='https://www.example.com/',
+    author='My Name',
+    author_email='yourname@example.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
