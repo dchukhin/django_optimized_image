b5201b43e0ea4e038893de41b34d0b9703e93587
Dmitriy Chukhin <dchukhin@gmail.com>
2017-01-13 06:47:05 -0500

    add GenericModel to not_optimized admin

--- not_optimized/admin.py
+++ not_optimized/admin.py
@@ -6,3 +6,7 @@ from . import models
 @admin.register(models.BlogPostOrSomething)
 class BlogPostOrSomethingAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(models.GenericModel)
+class GenericModel(admin.ModelAdmin):
+    pass
