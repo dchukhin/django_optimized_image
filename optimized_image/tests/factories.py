b86fdc236ae07dacef797696c2e3512d1d6c8aa0
Dmitriy Chukhin <dchukhin@gmail.com>
2018-01-13 09:50:57 -0500

    update tests for field and utils updates

--- optimized_image/tests/factories.py
+++ optimized_image/tests/factories.py
@@ -4,16 +4,6 @@ import factory.fuzzy
 from optimized_image.fields import OptimizedImageField
 
 
-class OptimizedNotOptimizedFactory(factory.django.DjangoModelFactory):
-    instance_model = factory.fuzzy.FuzzyText(length=8)
-    instance_pk = factory.fuzzy.FuzzyInteger(low=1)
-    field_name = factory.fuzzy.FuzzyText(length=8)
-    url = factory.fuzzy.FuzzyText()
-
-    class Meta:
-        model = 'optimized_image.OptimizedNotOptimized'
-
-
 class GenericModelFactory(factory.django.DjangoModelFactory):
     title = factory.fuzzy.FuzzyText(length=8)
     image = factory.django.ImageField(filename="image.png")
--- optimized_image/tests/test_fields.py
+++ optimized_image/tests/test_fields.py
@@ -1,5 +1,8 @@
-from unittest.mock import patch, Mock
+from factory.fuzzy import FuzzyText
+import io
+from unittest.mock import patch
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.test import TestCase
 
 from . import factories
@@ -8,31 +11,59 @@ from . import factories
 class TestOptimizedImageField(TestCase):
     """Test case for the OptimizedImageField."""
 
-    @patch('optimized_image.fields.save_to_s3')
-    def test_save_creates_optimized_image(self, mock_save_to_s3):
-        """Saving an image to an OptimizedImageField creates optimized image."""
-        tinify_return_object = Mock()
-        tinify_return_object.location = 'https://s3.amazonaws.com/testutils/optimized_images/image.png'
-        mock_save_to_s3.return_value = tinify_return_object
-
+    @patch('optimized_image.utils.optimize_from_buffer')
+    def test_save_does_not_create_optimized_image(self, mock_optimize_from_buffer):
+        """Saving an image to an OptimizedImageField does not call optimize_from_buffer()."""
         generic_model = factories.GenericModelFactory(
             title='Generic Model',
             image=None
         )
-        from optimized_image.models import OptimizedNotOptimized
-
-        # So far there is no optimized image, and mock_save_to_s3 has not been called
+        # So far there is no optimized image, and mock_optimize_from_buffer has not been called
         self.assertEqual(generic_model.image.name, None)
-        self.assertEqual(mock_save_to_s3.call_count, 0)
+        self.assertEqual(mock_optimize_from_buffer.call_count, 0)
 
-        import io
         new_io = io.StringIO()
         new_io.write('something')
-        from django.core.files.uploadedfile import InMemoryUploadedFile
-        new_file=InMemoryUploadedFile(new_io, 'image', 'static/images/vvv.png', 'image/jpeg', len(new_io.getvalue()), 'utf-8')
+        new_file = InMemoryUploadedFile(
+            new_io,
+            'image',
+            'static/images/{}.png'.format(FuzzyText().fuzz()),
+            'image/jpeg',
+            len(new_io.getvalue()),
+            'utf-8'
+        )
         generic_model.image = new_file
         generic_model.save()
 
         self.assertNotEqual(generic_model.image.name, None)
-        # The mock_save_to_s3 has been called once
-        self.assertEqual(mock_save_to_s3.call_count, 1)
+        # The mock_optimize_from_buffer still has not been called
+        self.assertEqual(mock_optimize_from_buffer.call_count, 0)
+
+    @patch('optimized_image.utils.optimize_from_buffer')
+    def test_save_form_data(self, mock_optimize_from_buffer):
+        """Calling save_form_data() on an OptimizedImageField calls optimize_from_buffer()."""
+        generic_model = factories.GenericModelFactory(
+            title='Generic Model',
+            image=None
+        )
+        # So far there is no optimized image, and mock_optimize_from_buffer has not been called
+        self.assertEqual(generic_model.image.name, None)
+        self.assertEqual(mock_optimize_from_buffer.call_count, 0)
+
+        new_io = io.StringIO()
+        new_io.write('something')
+        new_file = InMemoryUploadedFile(
+            new_io,
+            'image',
+            'static/images/{}.png'.format(FuzzyText().fuzz()),
+            'image/jpeg',
+            len(new_io.getvalue()),
+            'utf-8'
+        )
+
+        # Call save_form_data on the OptimizedImageField
+        generic_model.image.field.save_form_data(generic_model, new_file)
+
+        # Now the mock_optimize_from_buffer has been called once
+        self.assertEqual(mock_optimize_from_buffer.call_count, 1)
+        self.assertEqual(mock_optimize_from_buffer.call_args[0][0], new_file)
